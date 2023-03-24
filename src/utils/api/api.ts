0c6c086c37adcb0c838bd86042d31150eef80567
Usman Kiyani <usman.kiyani@ceative.co.uk>
2023-03-24 14:53:11 +0500

    api helper functions added

---
+++ src/utils/api/api.ts
@@ -0,0 +1,72 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+
+const baseURL = "https://example.com/api";
+
+const api: AxiosInstance = axios.create({
+  baseURL,
+});
+
+// Authorization header with the access token
+api.interceptors.request.use(
+    (config) => {
+      const accessToken = localStorage.getItem('accessToken');
+      if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+      }
+      return config;
+    },
+    (error) => {
+      console.error('Error sending request:', error);
+      return Promise.reject(error);
+    }
+  );
+
+export const request = async <T = any>(
+  config: AxiosRequestConfig
+): Promise<T> => {
+  const response: AxiosResponse<T> = await api.request<T>(config);
+  return response.data;
+};
+
+export const get = async <T = any>(
+  url: string,
+  config?: AxiosRequestConfig
+): Promise<T> => {
+  const response: AxiosResponse<T> = await api.get<T>(url, config);
+  return response.data;
+};
+
+export const post = async <T = any>(
+  url: string,
+  data?: any,
+  config?: AxiosRequestConfig
+): Promise<T> => {
+  const response: AxiosResponse<T> = await api.post<T>(url, data, config);
+  return response.data;
+};
+
+export const put = async <T = any>(
+  url: string,
+  data?: any,
+  config?: AxiosRequestConfig
+): Promise<T> => {
+  const response: AxiosResponse<T> = await api.put<T>(url, data, config);
+  return response.data;
+};
+
+export const patch = async <T = any>(
+  url: string,
+  data?: any,
+  config?: AxiosRequestConfig
+): Promise<T> => {
+  const response: AxiosResponse<T> = await api.patch<T>(url, data, config);
+  return response.data;
+};
+
+export const del = async <T = any>(
+  url: string,
+  config?: AxiosRequestConfig
+): Promise<T> => {
+  const response: AxiosResponse<T> = await api.delete<T>(url, config);
+  return response.data;
+};
