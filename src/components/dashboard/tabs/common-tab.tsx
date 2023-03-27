c3920a9f06434162a933134d2eed89d6afc2986a
alitechyyy <ali@techguysservices.com>
2023-03-27 21:46:57 +0500

    Grids Responsiveness and Final Commit before Todays Merge

--- src/components/dashboard/tabs/common-tab.tsx
+++ src/components/dashboard/tabs/common-tab.tsx
@@ -3,7 +3,7 @@ import CardComponent from "../card/card";
 
 const CommonTab: React.FC<any> = ({ data }) => {
   return (
-    <div className="flex gap-4 grid grid-cols-4">
+    <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-8 sm:gap-x-24 ">
       {data.map((card: { label: string; img: any }) => (
         <CardComponent label={card.label} img={card.img} />
       ))}
