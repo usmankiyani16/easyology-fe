4688e20f99bbc74cc8ae2c784f9f1c482262e45c
Usman Kiyani <usman.kiyani@ceative.co.uk>
2023-03-28 06:21:24 +0500

    changes

--- src/components/purchase-order/purchase-order.tsx
+++ src/components/purchase-order/purchase-order.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import POCard from "./po-card";
 
 const PurchaseOrder = () => {
-  return <div>PurchaseOrder</div>;
+  return (
+    <div>
+      <div className="flex items-center justify-between mt-3">
+        <h1>Purchase Order</h1>
+        <div>icon</div>
+      </div>
+      <POCard/>
+    </div>
+  );
 };
 
 export default PurchaseOrder;
