0d56f1ad3c04f50b46629cf790f581d5ea0997ee
Usman Kiyani <usman.kiyani@ceative.co.uk>
2023-04-10 23:43:08 +0500

    changes in bulk import mdl

--- src/components/Modals/add-po-modals/import-modal.tsx
+++ src/components/Modals/add-po-modals/import-modal.tsx
@@ -1,28 +1,65 @@
 import React, { useState } from "react";
 import "../modals.scss";
 
-import { Button, Modal } from "antd";
+import { Button, Form, Modal, Select } from "antd";
 
 import ExportExcel from "../../purchase-order/po-card/po-import/export-excel";
 import Dummy_Add_PO from "../../purchase-order/po-card/po-import/dummy-add-po";
 import BulkUpload from "../../purchase-order/po-card/po-import/bulk-upload";
+import { capitalize } from "../../../utils/functions/functions";
+import { useAppSelector } from "../../../store/store";
 // import UploadExcelTable from "../../purchase-order/po-card/po-import/import-excel";
 
-const Importmodal: React.FC<any> = ({importModalOpen,setImportModalOpen,}) => {
-
+const Importmodal: React.FC<any> = ({ importModalOpen, setImportModalOpen, }) => {
+  const { vendors } = useAppSelector((state) => state.vendors);
+  const onFinish = (values: any) => { }
   return (
     <div className="_modal_wrap">
       <Modal
-      
+
         width="400px"
         footer={false}
         centered
         open={importModalOpen}
         onCancel={() => setImportModalOpen(false)}
-        // destroyOnClose={true}
+      // destroyOnClose={true}
       >
         <h3 className="_modal_header text-red-500">Import PO In Bulk</h3>
+        <Form
+          // labelCol={{ span: 4 }}
+          wrapperCol={{ span: 14 }}
+          layout="vertical"
+          // style={{ maxWidth:  }}
+          onFinish={onFinish}
+          autoComplete="off"
+          className="mt-4"
+        >
+          <Form.Item
+            label="Select Vendor"
+            name="selectVendor"
+            required
+            tooltip="This is a required field"
+            rules={[
+              {
+                required: true,
+                // type: 'email',
+                message: "Required Field",
+              },
+              {
+                type: "string",
+              },
+            ]}
+          >
+            <Select className="_input" placeholder="Select Vendor">
+              {vendors?.map((vendor: any, index: number) => (
+                <Select.Option key={vendor?._id} value={vendor?._id}>
+                  {capitalize(vendor?.name)}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
 
+        </Form>
         <ExportExcel excelData={Dummy_Add_PO} fileName={"Export Excel"} />
 
         <BulkUpload />
